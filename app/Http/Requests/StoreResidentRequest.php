b10b6a3d2a11cbd90a5a2d58881bc64f877905aa
husni-robani <husnir2005@gmail.com>
2023-11-27 20:44:38 +0700

    create request for store resident and update resident

---
+++ app/Http/Requests/StoreResidentRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreResidentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'no_kk' => 'required|string|unique:residents,no_kk',
+            'house_number' => 'required|string',
+            'total_muzaki' => 'required|integer'
+        ];
+    }
+}

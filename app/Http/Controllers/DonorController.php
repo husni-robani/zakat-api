0d04c53c77c156e40f44144b2fe423129e841573
husni-robani <husnir2005@gmail.com>
2023-11-29 10:39:55 +0700

    create DonorController.php

---
+++ app/Http/Controllers/DonorController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreDonorRequest;
+use App\Http\Resources\DonorResource;
+use App\Models\Donor;
+use Illuminate\Http\Request;
+use App\Traits\ApiResponses;
+
+class DonorController extends Controller
+{
+    use ApiResponses;
+    public function index(){
+        try {
+            $donors = Donor::all();
+        }catch (\Exception$exception ){
+            return $this->responseFailed(
+                'Failed to get all donor',
+                400,
+                $exception->getMessage()
+            );
+        }
+        return $this->responseSuccess(
+            'Success to get all donor',
+            200,
+            DonorResource::collection($donors)
+        );
+    }
+}

2528f076b11ff00b49f8d7f3a74af6e04c0ab752
husni-robani <husnir2005@gmail.com>
2023-12-01 16:57:11 +0700

    create resource for Transaction model

---
+++ app/Http/Resources/TransactionResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TransactionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'amount' => $this->amount,
+            'description' => $this->description,
+            'donation_type' => $this->donationType->name,
+            'donor' => new DonorResource($this->donor),
+            'wallet' => $this->wallet->name,
+            'good_type' => $this->goodType->name
+        ];
+    }
+}

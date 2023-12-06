5080b26b6e61741c6004f2b2f0f437705d0e985c
husni-robani <husnir2005@gmail.com>
2023-12-06 20:04:36 +0700

    update method to be able sending email after transaction completed

--- app/Services/TransactionService.php
+++ app/Services/TransactionService.php
@@ -2,10 +2,12 @@
 
 namespace App\Services;
 
+use App\Mail\TransactionCompleted;
 use App\Models\Donor;
 use App\Models\Transaction;
 use App\Models\Wallet;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use function Laravel\Prompts\error;
 
 class TransactionService
@@ -35,7 +37,15 @@ class TransactionService
         $transaction->update([
             'completed' => true
         ]);
+
+        if ($transaction->donor->email){
+            $this->sendingEmailToDonor($transaction->donor->email);
+        }
         $transaction->wallet->addAmount($transaction->amount);
         return $transaction;
+    }
+
+    private function sendingEmailToDonor($email){
+        Mail::to($email)->send(new TransactionCompleted());
     }
 }

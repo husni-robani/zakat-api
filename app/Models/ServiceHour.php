30fd4797e83b2924cdf9a561c6f1b7c993e99cdc
husni-robani <husnir2005@gmail.com>
2023-12-06 14:49:57 +0700

    create model ServiceHour.php

---
+++ app/Models/ServiceHour.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ServiceHour extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['day', 'open', 'close'];
+}

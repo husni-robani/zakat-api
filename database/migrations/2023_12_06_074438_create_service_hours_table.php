3d52d0728132a6a162a1ebd7813a7de432b077de
husni-robani <husnir2005@gmail.com>
2023-12-06 14:49:37 +0700

    create table service_hours

---
+++ database/migrations/2023_12_06_074438_create_service_hours_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('service_hours', function (Blueprint $table) {
+            $table->id();
+            $table->enum('day', ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu']);
+            $table->time('open');
+            $table->time('close');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('service_hours');
+    }
+};

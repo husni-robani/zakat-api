6c3535943eeb1ee06cf0320e197ed67a7ae47196
husni-robani <husnir2005@gmail.com>
2023-12-06 15:01:28 +0700

    relocating some endpoint

--- routes/api.php
+++ routes/api.php
@@ -19,21 +19,22 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 });
 
 Route::middleware('auth')->group(function (){
-    Route::get('/residents', [\App\Http\Controllers\ResidentController::class, 'index']);
     Route::post('/residents', [\App\Http\Controllers\ResidentController::class, 'store']);
     Route::delete('/residents/{no_kk}', [\App\Http\Controllers\ResidentController::class, 'destroy']);
     Route::patch('/residents/{no_kk}', [\App\Http\Controllers\ResidentController::class, 'update']);
 
     Route::get('/guests', [\App\Http\Controllers\GuestController::class, 'index']);
-    Route::post('/guests', [\App\Http\Controllers\GuestController::class, 'store']);
     Route::get('/donors', [\App\Http\Controllers\DonorController::class, 'index']);
 
     Route::get('/transactions', [\App\Http\Controllers\TransactionController::class, 'index']);
-    Route::post('/transactions/create/{no_kk}', [\App\Http\Controllers\TransactionController::class, 'storeResidentTransaction']);
-    Route::post('/transactions/create', [\App\Http\Controllers\TransactionController::class, 'storeGuestTransaction']);
     Route::post('/transactions/completed/{id_transaction}', [\App\Http\Controllers\TransactionController::class, 'transactionCompleted']);
 });
 
+
+Route::get('/residents', [\App\Http\Controllers\ResidentController::class, 'index']);
+Route::post('/guests', [\App\Http\Controllers\GuestController::class, 'store']);
+Route::post('/transactions/create/{no_kk}', [\App\Http\Controllers\TransactionController::class, 'storeResidentTransaction']);
+Route::post('/transactions/create', [\App\Http\Controllers\TransactionController::class, 'storeGuestTransaction']);
 Route::get('/residents/{no_kk}', [\App\Http\Controllers\ResidentController::class, 'show']);
 Route::get('/donations', [\App\Http\Controllers\DonationTypeController::class, 'index']);
 

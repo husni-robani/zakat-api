54c1b80f9ded92ec367870fc183cf784407228b8
husni-robani <husnir2005@gmail.com>
2023-12-17 12:29:00 +0700

    create history resource

---
+++ app/Http/Resources/HistoryResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class HistoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'donation_type' => $this->donationType->name,
+            'donor_type' => class_basename(str_replace('//', '/', $this->donor->donorable_type)),
+            'no_kk' => $this->when($this->donor->donorable_type == 'App\\Models\\Resident', $this->donor->donorable->no_kk),
+            'donor_name' => $this->donor->name,
+            'wallet' => $this->wallet->name,
+            'good_type' => $this->goodType->name,
+            'amount' => $this->amount,
+            'completed_status' => $this->completed,
+            "created" => $this->created_at,
+            "updated" => $this->updated_at
+        ];
+    }
+}

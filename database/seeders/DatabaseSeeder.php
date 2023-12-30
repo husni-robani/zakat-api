f9d82276c40c3a88de3fd02210f363ee79647498
husni-robani <husnir2005@gmail.com>
2023-12-30 19:29:08 +0700

    Update seeder that running on DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,12 +2,8 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\DonationType;
-use App\Models\GoodType;
 use App\Models\ServiceHour;
 use App\Models\User;
-use App\Models\Wallet;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -32,9 +28,11 @@ class DatabaseSeeder extends Seeder
         (new DonationTypeSeeder())->run();
         (new GoodTypeSeeder())->run();
         (new WalletSeeder())->run();
-        (new ResidentSeeder())->run();
-        (new DonorSeeder())->run();
         (new TransactionSeeder())->run();
+
+//        (new DonorSeeder())->run();
+//        (new ResidentSeeder())->run();
+//        (new GuestSeeder())->run();
 //        (new DistributionSeeder())->run();
     }
 }

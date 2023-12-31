1ab96e8adb4147488f062ac9b233eb7ad8250a8b
husni-robani <husnir2005@gmail.com>
2023-12-31 23:39:11 +0700

    fixing a lil bug on transaction factory

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Donor;
 use App\Models\ServiceHour;
+use App\Models\Transaction;
 use App\Models\Wallet;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -19,16 +20,16 @@ class TransactionFactory extends Factory
      */
     public function definition(): array
     {
-        $goodTypes = [1, 2];
+        $good_types_id = $this->faker->randomElement([1, 2]);
         $donationTypes = [1, 2, 3, 4];
-        $amount = $this->faker->randomElement($goodTypes) === 1
-            ? $this->faker->numberBetween(1, 999)
+        $amount = $good_types_id == 1
+            ? $this->faker->numberBetween(1, 1000)
             : $this->faker->numberBetween(1000, 999999999);
-        $walletsId = Wallet::where('good_types_id', $goodTypes)->where('donation_types_id', $donationTypes)->firstOrFail();
+        $walletsId = Wallet::where('good_types_id', $good_types_id)->where('donation_types_id', $donationTypes)->firstOrFail();
         $donors_id = Donor::factory()->count(1)->create();
 
         return [
-            'good_types_id' => $this->faker->randomElement($goodTypes),
+            'good_types_id' => $good_types_id,
             'donation_types_id' => $this->faker->randomElement($donationTypes),
             'description' => $this->faker->paragraphs(5, true),
             'amount' => $amount,
@@ -37,5 +38,17 @@ class TransactionFactory extends Factory
             'service_hours_id' => ServiceHour::inRandomOrder()->first(),
             'donors_id' => $donors_id->value('id')
         ];
+
+
+    }
+
+    public function configure()
+    {
+        return $this->afterCreating(function (Transaction $transaction){
+            if ($transaction->completed){
+                $transaction->wallet->amount += $transaction->amount;
+                $transaction->wallet->save();
+            }
+        });
     }
 }

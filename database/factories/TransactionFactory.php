693720a41404690b3fa9b60396b610778199ead0
husni-robani <husnir2005@gmail.com>
2024-01-02 15:47:43 +0700

    update TransactionFactory.php to add invoice number

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -6,13 +6,17 @@ use App\Models\Donor;
 use App\Models\ServiceHour;
 use App\Models\Transaction;
 use App\Models\Wallet;
+use App\Services\TransactionService;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Collection;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Transaction>
  */
 class TransactionFactory extends Factory
 {
+
+    protected $model = Transaction::class;
     /**
      * Define the model's default state.
      *
@@ -20,27 +24,40 @@ class TransactionFactory extends Factory
      */
     public function definition(): array
     {
+
         $good_types_id = $this->faker->randomElement([1, 2]);
-        $donationTypes = [1, 2, 3, 4];
+        $donationTypeId = $this->faker->randomElement([1, 2, 3, 4]);
         $amount = $good_types_id == 1
             ? $this->faker->numberBetween(1, 1000)
             : $this->faker->numberBetween(1000, 999999999);
-        $walletsId = Wallet::where('good_types_id', $good_types_id)->where('donation_types_id', $donationTypes)->firstOrFail();
+        $walletsId = Wallet::where('good_types_id', $good_types_id)->where('donation_types_id', $donationTypeId)->firstOrFail();
         $donors_id = Donor::factory()->count(1)->create();
-
+        $date = $this->faker->dateTimeBetween('-4 years', 'now');
+        static $counter = 1;
         return [
+            'invoice_number' => $this->generateInvoiceNumber($donationTypeId, $counter++, $date),
             'good_types_id' => $good_types_id,
-            'donation_types_id' => $this->faker->randomElement($donationTypes),
+            'donation_types_id' => $donationTypeId,
             'description' => $this->faker->sentence(5),
             'amount' => $amount,
             'completed' => $this->faker->boolean(),
             'wallets_id' => $walletsId,
             'donors_id' => $donors_id->value('id'),
-            'created_at' => $this->faker->dateTimeBetween('-4 years', 'now')
+            'created_at' => $date
         ];
 
 
     }
+    public function generateInvoiceNumber($donationTypeId, $uniqueNumber, $date) : string{
+        return match ($donationTypeId) {
+            1 => 'INV-FTR-' . $date->format('Y') . '-' . $uniqueNumber,
+            2 => 'INV-MAL-' . $date->format('Y') . '-' . $uniqueNumber,
+            3 => 'INV-FDY-' . $date->format('Y') . '-' . $uniqueNumber,
+            4 => 'INV-SDK-' . $date->format('Y') . '-' . $uniqueNumber,
+            default => 'INV-' . $date->format('Y') . '-' . $uniqueNumber,
+        };
+    }
+
 
     public function configure()
     {

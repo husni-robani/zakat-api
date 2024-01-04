305eed085a0c4a73836d4f98cbb723a52b6bb7b4
husni-robani <husnir2005@gmail.com>
2024-01-05 02:08:03 +0700

    change query parameter get history

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -11,13 +11,41 @@ use Illuminate\Http\Request;
 class HistoryController extends Controller
 {
     use ApiResponses;
-    public function historyTransaction(bool $paginated = false){
+    public function historyTransaction(Request $request){
         try {
-            $history = Transaction::when($paginated, function (){
-                return Transaction::with('wallet', 'goodType', 'donationType', 'donor.donorable')->paginate(10);
-            }, function (){
-                return Transaction::with('wallet', 'goodType', 'donationType', 'donor.donorable')->get();
+            $history = Transaction::query();
+
+            $history->when($request->query('q'), function ($query) use ($request){
+                $query->where('invoice_number', 'like', '%'.$request->query('q').'%')
+                    ->orWhereHas('donor', function ($query) use ($request){
+                        $query->where('name', 'like', '%'.$request->query('q').'%')
+                            ->orWhereHasMorph('donorable', ['App\Models\Resident'], function ($query) use ($request){
+                                $query->where('house_number', 'like', '%' . $request->query('q') . '%')
+                                    ->orWhere('no_kk', 'like', '%' . $request->query('q') . '%');
+                            });
+                    });
             });
+
+            $history->when($request->query('good_type_id'), fn($query) => $query->where('good_types_id', $request->query('good_type_id')));
+
+            $history->when($request->query('donation_type_id'), fn($query) => $query->where('donation_types_id', $request->query('donation_type_id')));
+
+            $history->when($request->query('wallet_id'), fn($query) => $query->where('wallets_id', $request->query('wallet_id')));
+
+            $history->when($request->query('donor_type') === 'resident', function ($query) use ($request){
+                $query->whereHas('donor', function ($query) use ($request){
+                   $query->where('donorable_type', 'App\Models\Resident');
+                });
+            });
+
+            $history->when($request->query('donor_type') === 'guest', function ($query) use ($request){
+                $query->whereHas('donor', function ($query) use ($request){
+                    $query->where('donorable_type', 'App\Models\Guest');
+                });
+            });
+
+            $history = $request->query('paginated') ? $history->orderByDesc('created_at')->paginate($request->query('paginated')) : $history->orderByDesc('created_at')->get();
+
         }catch (ModelNotFoundException | \Exception $exception){
             return $this->responseFailed(
                 'get history transcation failed',
--- app/Http/Resources/HistoryResource.php
+++ app/Http/Resources/HistoryResource.php
@@ -15,9 +15,11 @@ class HistoryResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'invoice_number' => $this->invoice_number,
             'donation_type' => $this->donationType->name,
             'donor_type' => class_basename(str_replace('//', '/', $this->donor->donorable_type)),
             'no_kk' => $this->when($this->donor->donorable_type == 'App\\Models\\Resident', $this->donor->donorable->no_kk),
+            'house_number' => $this->when($this->donor->donorable_type == 'App\\Models\\Resident', $this->donor->donorable->house_number),
             'donor_name' => $this->donor->name,
             'wallet' => $this->wallet->name,
             'good_type' => $this->goodType->name,

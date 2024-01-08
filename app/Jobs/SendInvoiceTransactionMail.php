50900a30e5bafa3b5796e9e194e40714e7cbc412
husni-robani <husnir2005@gmail.com>
2024-01-08 22:41:52 +0700

    create job SendInvoiceTransactionMail.php for sending invoice transaction email

---
+++ app/Jobs/SendInvoiceTransactionMail.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Http\Resources\TransactionResource;
+use App\Mail\TransactionInvoiceMail;
+use App\Models\Transaction;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class SendInvoiceTransactionMail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    private Transaction $transaction;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(Transaction $transaction)
+    {
+        $this->transaction = $transaction;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Mail::to($this->transaction->donor->email)->send(new TransactionInvoiceMail(new TransactionResource($this->transaction)));
+    }
+}
